f6c55892cebd406a3ee3fd6563b7d19880855880
Josep69 <jubet_68@hotmail.com>
2025-07-03 17:23:03 +0200

    Curate the functions: remove unused ones by MATLAB project analysis

--- Length_determination/Code_functions/to_3.m
+++
@@ -1,4 +0,0 @@
-%% Convert the matrix to a 3 dimensional matrix
-function [matriu_3] = to_3(matriu_1)
-    matriu_3 = cat(3, matriu_1, matriu_1, matriu_1);
-end
--- Length_determination/Code_functions/vistrel.m
+++
@@ -1,6 +0,0 @@
-function vistrel(Strel_AAA)
-    % Example: Strel_AAA = strel('disk', 5);
-    % From: https://es.mathworks.com/matlabcentral/answers/368927-how-to-display-the-object-created-with-function-strel
-    Strel_AAA.Neighborhood
-    image(Strel_AAA.Neighborhood); colormap(gray(2));
-end
